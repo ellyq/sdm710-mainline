torvalds/linux

commit a0ce874cfaaab9792d657440b9d050e2112f6e4d

ASoC: ops: improve snd_soc_get_volsw
* clamp the values if the register value read is
  out of range

Signed-off-by: Niranjan H Y <niranjan.hy@ti.com>
[This patch originally had two changes in it, I removed a second buggy
 one -- broonie]
--
v5:
 - remove clamp parameter
 - move the boundary check after sign-bit extension
Link: https://patch.msgid.link/20250912083624.804-1-niranjan.hy@ti.com
Signed-off-by: Mark Brown <broonie@kernel.org>

sound/soc/soc-ops.c
@@ -118,6 +118,7 @@ static int soc_mixer_reg_to_ctl(struct soc_mixer_control *mc, unsigned int reg_v
 	if (mc->sign_bit)
 		val = sign_extend32(val, mc->sign_bit);
 
+	val = clamp(val, mc->min, mc->max);
 	val -= mc->min;
 
 	if (mc->invert)

ellyq/sdm710-mainline

commit 084a4b0691909e5ae6d813dc03e1dcd533cc1891

ASoC: ops: fix clamp maximum for SX controls
Some controls are SX. For SX controls, the register has a range of
mc->min to mc->min + mc->max. Some controls (like the Digital PCM Volume
in CS35L36) may have mc->min > mc->max, which can force all readings to
be the same value. Fix this by using the max parameter, which is
properly adjusted for SX and non-SX controls.

Fixes: a0ce874cfaaa ("ASoC: ops: improve snd_soc_get_volsw")
Signed-off-by: Richard Acayan <mailingradian@gmail.com>

sound/soc/soc-ops.c
@@ -118,7 +118,7 @@ static int soc_mixer_reg_to_ctl(struct soc_mixer_control *mc, unsigned int reg_v
 	if (mc->sign_bit)
 		val = sign_extend32(val, mc->sign_bit);
 
-	val = clamp(val, mc->min, mc->max);
+	val = clamp(val, mc->min, mc->min + max);
 	val -= mc->min;
 
 	if (mc->invert)